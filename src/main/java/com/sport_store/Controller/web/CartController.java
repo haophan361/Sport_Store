30ee42a162f0c6d8219cd959d545ec3a97bca742
Nguyễn Hoài Bảo <22110108@student.hcmute.edu.vn>
2025-04-05 09:46:47 +0700

    update CartController.java

---
+++ src/main/java/com/sport_store/Controller/web/CartController.java
@@ -0,0 +1,20 @@
+package com.sport_store.Controller.web;
+
+import com.sport_store.Util.LoadUser;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+
+public class CartController {
+    private final LoadUser login;
+
+    @GetMapping("/customer/cart")
+    public String shop(Model model, HttpSession session) {
+        return "/customer/cart";
+    }
+}
