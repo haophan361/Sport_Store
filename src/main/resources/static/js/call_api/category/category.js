cc6f817b9189d56eeef003d6e0923157a3c4556e
My Dung <mydung101204@gmail.com>
2025-04-21 15:36:05 +0700

    nut xoa trong loai, thuong hieu, mau, giam gia

--- src/main/resources/static/js/call_api/category/category.js
+++ src/main/resources/static/js/call_api/category/category.js
@@ -14,11 +14,55 @@ function saveCategory() {
 
 function fetchCategory() {
     fetch("/getAllCategory")
-        .then(response => {
-            if (response.ok) return response.json();
-        })
-        .then(data => {
-            populateSelect("category_id", data, "Loại", "category_id", "category_name");
-        })
-}
+      .then((response) => response.json())
+      .then((data) => {
+        const dropdown = document.getElementById("customCategoryDropdown");
+        dropdown.innerHTML = "";
+  
+        data.forEach((item) => {
+          const div = document.createElement("div");
+          div.className = "dropdown-item d-flex justify-content-between align-items-center";
+          div.innerHTML = `
+            <span>${item.category_name}</span>
+            <i class="fas fa-trash-alt text-danger" style="cursor:pointer;" onclick="deleteCategory(${item.category_id})"></i>
+          `;
+          dropdown.appendChild(div);
+        });
+  
+        const addNew = document.createElement("div");
+        addNew.className = "dropdown-item text-primary";
+        addNew.setAttribute("data-toggle", "modal");
+        addNew.setAttribute("data-target", "#addCategoryModal");
+        addNew.innerHTML = `<i class="fa fa-plus"></i> Thêm Loại mới`;
+        dropdown.appendChild(addNew);
+      });
+  }
+  
+  function fetchBrand() {
+    fetch("/getAllBrand")
+      .then((response) => response.json())
+      .then((data) => {
+        const dropdown = document.getElementById("customBrandDropdown");
+        dropdown.innerHTML = "";
+  
+        data.forEach((item) => {
+          const div = document.createElement("div");
+          div.className = "dropdown-item d-flex justify-content-between align-items-center";
+          div.innerHTML = `
+            <span>${item.brand_name}</span>
+            <i class="fas fa-trash-alt text-danger" style="cursor:pointer;" onclick="deleteBrand(${item.brand_id})"></i>
+          `;
+          dropdown.appendChild(div);
+        });
+  
+        const addNew = document.createElement("div");
+        addNew.className = "dropdown-item text-primary";
+        addNew.setAttribute("data-toggle", "modal");
+        addNew.setAttribute("data-target", "#addBrandModal");
+        addNew.innerHTML = `<i class="fa fa-plus"></i> Thêm Thương hiệu mới`;
+        dropdown.appendChild(addNew);
+      });
+  }
+  
+
 
